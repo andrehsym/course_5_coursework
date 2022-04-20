60d7348569b98a6560c49f6ee1c3b3e5b29ea233
andrehsym <kochetkovandre1993@gmail.com>
2022-04-20 20:29:33 +0300

    1 commit

---
+++ src/main/java/com/course_5/course_5_coursework/Course5CourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.course_5.course_5_coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course5CourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course5CourseworkApplication.class, args);
+    }
+
+}
